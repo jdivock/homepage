a51ca7d0d6acad69539a22b763501bbc13475602
Jason Divock <jdivock@hugeinc.com>
2014-06-15 07:28:20 -0400

    looks good, ship it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,45 +2,10 @@
     'use strict';
 
     $(document).ready(function() {
-        // Init Skrollr
-        var s = skrollr.init({
-            render: function(data) {
-                //Debugging - Log the current scroll position.
-                // console.log(data.curTop);
-            }
-        });
 
-        window.onload = function() {
-            setSize(s);
-        };
-
-        $(window).resize(function() {
-            setSize(s);
-        });
     });
 
-    function setSize(s) {
-        $('section').height($(window).innerHeight() * 2);
-        $('section').first().height($(window).innerHeight());
-        $('section').last().height($(window).innerHeight());
-        $('section#photos').height($(window).innerHeight() * 5);
-        s.refresh();
-    }
+
 
 
 })();
-
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-});
